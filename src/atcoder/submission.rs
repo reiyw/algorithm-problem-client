324184ecefa7c331a8585b8762bd5b3972c1e570
reiyw <reiyw.setuve@gmail.com>
2020-08-11 21:35:11 +0900

    don't unwrap

--- src/atcoder/submission.rs
+++ src/atcoder/submission.rs
@@ -149,7 +149,8 @@ pub(super) async fn scrape_submission_code(contest_id: &str, submission_id: u64)
     let html = util::get_html(&url)
         .await
         .map_err(|_| Error::HtmlParseError)?;
-    let code_selector = Selector::parse(r#"pre[id="submission-code"]"#).unwrap();
+    let code_selector =
+        Selector::parse(r#"pre[id="submission-code"]"#).map_err(|_| Error::HtmlParseError)?;
     let code = Html::parse_document(&html)
         .select(&code_selector)
         .next()
